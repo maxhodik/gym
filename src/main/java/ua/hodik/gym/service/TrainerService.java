6a9e7030d571fc1006ebadb01782e81944769705
m-god <m-godik@ukr.net>
2024-09-01 17:06:58 +0300

    added TrainerService

---
+++ src/main/java/ua/hodik/gym/service/TrainerService.java
@@ -0,0 +1,15 @@
+package ua.hodik.gym.service;
+
+import ua.hodik.gym.model.Trainer;
+
+import java.util.Optional;
+
+public interface TrainerService {
+    Optional<Trainer> create(Trainer trainer);
+
+    Optional<Trainer> update(Trainer trainer, int id);
+
+    boolean delete(int id);
+
+    Optional<Trainer> findById(int id);
+}
