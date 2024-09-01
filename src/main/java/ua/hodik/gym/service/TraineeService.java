0795371a24d335d947ca4597e41bb4392ad4c4af
m-god <m-godik@ukr.net>
2024-09-01 17:04:33 +0300

    added TraineeService

---
+++ src/main/java/ua/hodik/gym/service/TraineeService.java
@@ -0,0 +1,15 @@
+package ua.hodik.gym.service;
+
+import ua.hodik.gym.model.Trainee;
+
+import java.util.Optional;
+
+public interface TraineeService {
+    Optional<Trainee> create(Trainee trainee);
+
+    Optional<Trainee> update(Trainee trainee, int id);
+
+    boolean delete(int id);
+
+    Optional<Trainee> findById(int id);
+}
