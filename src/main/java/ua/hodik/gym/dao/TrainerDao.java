42900eba7ba033c0b4ef4ad93a0b7ba2539e7419
m-god <m-godik@ukr.net>
2024-09-01 17:30:04 +0300

    added TrainerDao

---
+++ src/main/java/ua/hodik/gym/dao/TrainerDao.java
@@ -0,0 +1,22 @@
+package ua.hodik.gym.dao;
+
+import ua.hodik.gym.model.Trainer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TrainerDao {
+    Optional<Trainer> add(Trainer trainer);
+
+    Optional<Trainer> update(Trainer trainer, int id);
+
+    boolean delete(int id);
+
+    Optional<Trainer> getById(int id);
+
+    List<Trainer> getAllTrainers();
+
+    List<Trainer> getAllTrainersByUserName();
+
+    int getMaxId();
+}
