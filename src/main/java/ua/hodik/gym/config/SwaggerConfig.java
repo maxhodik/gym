02d6a1bf9fcbaf33f3081d22519d2614241858db
m-god <m-godik@ukr.net>
2024-11-05 12:32:17 +0200

    refactoring SwaggerConfig

--- src/main/java/ua/hodik/gym/config/SwaggerConfig.java
+++ src/main/java/ua/hodik/gym/config/SwaggerConfig.java
@@ -6,8 +6,9 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class SwaggerConfig {
 
     @Bean
@@ -16,6 +17,6 @@ public class SwaggerConfig {
                 .info(new Info().title("JavaInUse Authentication Service"))
                 .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
                 .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
-                        .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("basic")));
+                        .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 }
